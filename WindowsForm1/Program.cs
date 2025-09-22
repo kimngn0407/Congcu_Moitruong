aed907e811ba21d02c712db6ffb10372f4b06d7d
NongHoi <nonghoi2004@gmail.com>
2025-09-22 09:50:46 +0700

    add btn login

--- WindowsForm1/Program.cs
+++ WindowsForm1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsForm1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new d());
         }
     }
 }
